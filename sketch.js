487a962f285142bc9c0e2977b3ddfc1a8d66d703
Mayara-Koch <128334306+Mayara-Koch@users.noreply.github.com>
2023-11-17 22:42:46 -0300

    Adicionando arquivo ccs e js

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let cor;
+
+let circuloX; // horizontal
+
+let circuloY; // vertical
+
+function setup() {
+
+
+circuloX = [0, 0, 0];
+
+circuloY = [random(height), random(height), random(height)];
+
+createCanvas(400, 400); // width x height
+
+background(color(100, 0, 0));
+
+cor = color(random(255), random(255), random(255));
+
+circuloX = [0, 0];
+
+circuloY = [random(height), random(height)];
+
+}
+
+
+function draw() {
+
+fill(cor);
+
+for (let contador in circuloX) {
+
+circle(circuloX[contador], circuloY[contador], 50);
+
+circuloX[contador] += random(0, 3);
+
+circuloY[contador] += random(-3, 3);
+
+}
+
+// código do mouseIsPressed suprimido
+
+}
